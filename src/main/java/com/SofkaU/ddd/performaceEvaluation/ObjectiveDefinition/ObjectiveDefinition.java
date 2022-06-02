188027a2493c503cb1ae732eb9adf6aefb71c07b
Jehison Galarza <jehisonsc045@gmail.com>
2022-06-01 22:44:16 -0500

    DDDPetProject - Command methods in aggregate root added

--- src/main/java/com/SofkaU/ddd/performaceEvaluation/ObjectiveDefinition/ObjectiveDefinition.java
+++ src/main/java/com/SofkaU/ddd/performaceEvaluation/ObjectiveDefinition/ObjectiveDefinition.java
@@ -2,13 +2,12 @@ package com.SofkaU.ddd.performaceEvaluation.ObjectiveDefinition;
 
 import co.com.sofka.domain.generic.AggregateEvent;
 import co.com.sofka.domain.generic.DomainEvent;
-import com.SofkaU.ddd.performaceEvaluation.ObjectiveDefinition.events.BossAdded;
-import com.SofkaU.ddd.performaceEvaluation.ObjectiveDefinition.events.EmployeeAdded;
-import com.SofkaU.ddd.performaceEvaluation.ObjectiveDefinition.events.ObjectiveDefinitionCreated;
+import com.SofkaU.ddd.performaceEvaluation.ObjectiveDefinition.events.*;
 import com.SofkaU.ddd.performaceEvaluation.ObjectiveDefinition.value.*;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class ObjectiveDefinition extends AggregateEvent<ObjectiveDefinitionID> {
@@ -53,4 +52,125 @@ public class ObjectiveDefinition extends AggregateEvent<ObjectiveDefinitionID> {
 
     }
 
+    public void AddEvaluation(ObjectiveDefinitionID objectiveDefinitionID, EvaluationID evaluationID, Deadline deadline, AreaObjective areaObjective, GlobalObjective globalObjective){
+        Objects.requireNonNull(objectiveDefinitionID);
+        Objects.requireNonNull(evaluationID);
+        Objects.requireNonNull(deadline);
+        Objects.requireNonNull(areaObjective);
+        Objects.requireNonNull(globalObjective);
+        appendChange(new EvaluationAdded(objectiveDefinitionID, evaluationID, deadline, areaObjective, globalObjective)).apply();
+    }
+
+    public void UpdateBossDepartment(ObjectiveDefinitionID objectiveDefinitionID, BossID bossID, Department department){
+        Objects.requireNonNull(objectiveDefinitionID);
+        Objects.requireNonNull(bossID);
+        Objects.requireNonNull(department);
+        appendChange(new BossDepartmentUpdated(objectiveDefinitionID, bossID, department)).apply();
+    }
+
+    public void UpdateBossName(ObjectiveDefinitionID objectiveDefinitionID, BossID bossID, Name name){
+        Objects.requireNonNull(objectiveDefinitionID);
+        Objects.requireNonNull(bossID);
+        Objects.requireNonNull(name);
+        appendChange(new BossNameUpdated(objectiveDefinitionID, bossID, name)).apply();
+    }
+
+    public void UpdateEmployeeDeadline(ObjectiveDefinitionID objectiveDefinitionID, EmployeeID employeeID, Deadline deadline){
+        Objects.requireNonNull(objectiveDefinitionID);
+        Objects.requireNonNull(employeeID);
+        Objects.requireNonNull(deadline);
+        appendChange(new EmployeeDeadlineUpdated(objectiveDefinitionID, employeeID, deadline)).apply();
+    }
+
+    public void UpdateEmployeeDepartment(ObjectiveDefinitionID objectiveDefinitionID, EmployeeID employeeID, Department department){
+        Objects.requireNonNull(objectiveDefinitionID);
+        Objects.requireNonNull(employeeID);
+        Objects.requireNonNull(department);
+        appendChange(new EmployeeDepartmentUpdated(objectiveDefinitionID, employeeID, department)).apply();
+    }
+
+    public void UpdateEmployeeMetric(ObjectiveDefinitionID objectiveDefinitionID, EmployeeID employeeID, Metric metric){
+        Objects.requireNonNull(objectiveDefinitionID);
+        Objects.requireNonNull(employeeID);
+        Objects.requireNonNull(metric);
+        appendChange(new EmployeeMetricUpdated(objectiveDefinitionID, employeeID, metric)).apply();
+    }
+
+    public void UpdateEmployeeName(ObjectiveDefinitionID objectiveDefinitionID, EmployeeID employeeID, Name name){
+        Objects.requireNonNull(objectiveDefinitionID);
+        Objects.requireNonNull(employeeID);
+        Objects.requireNonNull(name);
+        appendChange(new EmployeeNameUpdated(objectiveDefinitionID, employeeID, name)).apply();
+    }
+
+    public void UpdateEmployeeObjective(ObjectiveDefinitionID objectiveDefinitionID, EmployeeID employeeID, Objective objective){
+        Objects.requireNonNull(objectiveDefinitionID);
+        Objects.requireNonNull(employeeID);
+        Objects.requireNonNull(objective);
+        appendChange(new EmployeeObjectiveUpdated(objectiveDefinitionID, employeeID, objective)).apply();
+    }
+
+    public void UpdateEvaluationAreaObjective(ObjectiveDefinitionID objectiveDefinitionID, EvaluationID evaluationID, AreaObjective areaObjective){
+        Objects.requireNonNull(objectiveDefinitionID);
+        Objects.requireNonNull(evaluationID);
+        Objects.requireNonNull(areaObjective);
+        appendChange(new EvaluationAreaObjectiveUpdated(objectiveDefinitionID, evaluationID, areaObjective)).apply();
+    }
+
+    public void UpdateEvaluationDeadline(ObjectiveDefinitionID objectiveDefinitionID, EvaluationID evaluationID, Deadline deadline){
+        Objects.requireNonNull(objectiveDefinitionID);
+        Objects.requireNonNull(evaluationID);
+        Objects.requireNonNull(deadline);
+        appendChange(new EvaluationDeadlineUpdated(objectiveDefinitionID, evaluationID, deadline)).apply();
+    }
+
+    public void UpdateEvaluationGlobalObjective(ObjectiveDefinitionID objectiveDefinitionID, EvaluationID evaluationID, GlobalObjective globalObjective){
+        Objects.requireNonNull(objectiveDefinitionID);
+        Objects.requireNonNull(evaluationID);
+        Objects.requireNonNull(globalObjective);
+        appendChange(new EvaluationGlobalObjectiveUpdated(objectiveDefinitionID, evaluationID, globalObjective)).apply();
+    }
+
+    public void UpdateEvaluationPeriod(ObjectiveDefinitionID objectiveDefinitionID, EvaluationPeriod evaluationPeriod){
+        Objects.requireNonNull(objectiveDefinitionID);
+        Objects.requireNonNull(evaluationPeriod);
+        appendChange(new EvaluationPeriodUpdated(objectiveDefinitionID, evaluationPeriod)).apply();
+    }
+
+    protected Optional<Boss> bossById(BossID bossID){
+        return bosses
+                .stream()
+                .filter(boss -> boss.identity().equals(bossID))
+                .findFirst();
+    }
+
+    protected Optional<Employee> employeeById(EmployeeID employeeID){
+        return employees
+                .stream()
+                .filter(employee -> employee.identity().equals(employee))
+                .findFirst();
+    }
+
+    protected Optional<Evaluation> bossById(EvaluationID evaluationID){
+        return evaluations
+                .stream()
+                .filter(evaluation -> evaluation.identity().equals(evaluationID))
+                .findFirst();
+    }
+
+    public EvaluationPeriod getEvaluationPeriod() {
+        return evaluationPeriod;
+    }
+
+    public Set<Employee> getEmployees() {
+        return employees;
+    }
+
+    public Set<Boss> getBosses() {
+        return bosses;
+    }
+
+    public Set<Evaluation> getEvaluations() {
+        return evaluations;
+    }
 }
